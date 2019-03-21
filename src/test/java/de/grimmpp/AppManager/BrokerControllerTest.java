378f27d7b4d8889cffea949c9767e30b850b12e4
Philipp Grimm <grimm.philipp@gmail.com>
2019-03-21 23:25:17 +0100

    brokerController tests added

--- src/test/java/de/grimmpp/AppManager/BrokerControllerTest.java
+++ src/test/java/de/grimmpp/AppManager/BrokerControllerTest.java
@@ -7,11 +7,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.servicebroker.model.binding.BindResource;
+import org.springframework.cloud.servicebroker.model.binding.CreateServiceInstanceBindingRequest;
+import org.springframework.cloud.servicebroker.model.binding.DeleteServiceInstanceBindingRequest;
 import org.springframework.cloud.servicebroker.model.instance.CreateServiceInstanceRequest;
 import org.springframework.cloud.servicebroker.model.instance.CreateServiceInstanceResponse;
+import org.springframework.cloud.servicebroker.model.instance.DeleteServiceInstanceRequest;
+import org.springframework.cloud.servicebroker.model.instance.UpdateServiceInstanceRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RunWith(SpringRunner.class)
@@ -31,16 +37,18 @@ public class BrokerControllerTest {
     private BrokerController brokerController;
 
 
+    private String siId = UUID.randomUUID().toString();
+    private String bId = UUID.randomUUID().toString();
+    private String appId = UUID.randomUUID().toString();
+    private String planId = UUID.randomUUID().toString();
+    private String orgId = UUID.randomUUID().toString();
+    private String spaceId = UUID.randomUUID().toString();
+    private String time = "1w 3d 5m";
+
+
     @Test
     public void serviceInstanceProvisioningTest() {
         cleanDatabase();
-
-        String siId = UUID.randomUUID().toString();
-        String planId = UUID.randomUUID().toString();
-        String orgId = UUID.randomUUID().toString();
-        String spaceId = UUID.randomUUID().toString();
-        String time = "1w 3d 5m";
-
 
         CreateServiceInstanceRequest request = CreateServiceInstanceRequest
                 .builder()
@@ -62,6 +70,105 @@ public class BrokerControllerTest {
         Assert.assertEquals(siId, parameterList.get(0).getReference());
         Assert.assertEquals("time", parameterList.get(0).getKey());
         Assert.assertEquals(time, parameterList.get(0).getValue());
+    }
+
+    @Test
+    public void bindingProvisioningTest() {
+        serviceInstanceProvisioningTest();
+
+        CreateServiceInstanceBindingRequest request = CreateServiceInstanceBindingRequest.builder()
+                .bindingId(bId)
+                .serviceInstanceId(siId)
+                .bindResource(BindResource.builder()
+                        .appGuid(appId)
+                        .build())
+                .parameters("time", time)
+                .build();
+
+        brokerController.createServiceInstanceBinding(request);
+
+        Binding b = bindingRepository.findById(bId).get();
+        Assert.assertNotNull(b);
+        Assert.assertEquals(bId, b.getBindingId());
+        Assert.assertEquals(appId, b.getApplicationId());
+        Assert.assertEquals(siId, b.getServiceInstanceId());
+
+        List<Parameter> parameterList = parameterRepository.findParametersByReference(bId);
+        Assert.assertEquals(1, parameterList.size());
+        Assert.assertEquals(bId, parameterList.get(0).getReference());
+        Assert.assertEquals("time", parameterList.get(0).getKey());
+        Assert.assertEquals(time, parameterList.get(0).getValue());
+    }
+
+    @Test
+    public void bindingDeprovisioningTest() {
+        bindingProvisioningTest();
+
+        DeleteServiceInstanceBindingRequest request = DeleteServiceInstanceBindingRequest.builder()
+                .bindingId(bId)
+                .serviceInstanceId(siId)
+                .build();
+
+        brokerController.deleteServiceInstanceBinding(request);
+
+        Optional<Binding> b = bindingRepository.findById(bId);
+        Assert.assertTrue( !b.isPresent() );
+
+        List<Parameter> parameterList = parameterRepository.findParametersByReference(bId);
+        Assert.assertTrue( parameterList.isEmpty() );
+
+        ServiceInstance si = serviceInstanceRepository.findByServiceInstanceId(siId);
+        Assert.assertNotNull(si);
+        Assert.assertEquals(siId, si.getServiceInstanceId());
+        Assert.assertEquals(planId, si.getServicePlanId());
+    }
+
+    @Test
+    public void serviceInstanceDeleteTest() {
+        bindingDeprovisioningTest();
+
+        DeleteServiceInstanceRequest request = DeleteServiceInstanceRequest.builder()
+                .planId(planId)
+                .serviceInstanceId(siId)
+                .build();
+
+        brokerController.deleteServiceInstance(request);
+
+        ServiceInstance si = serviceInstanceRepository.findByServiceInstanceId(siId);
+        Assert.assertTrue(si == null);
+
+        List<Parameter> parameterList = parameterRepository.findParametersByReference(siId);
+        Assert.assertTrue( parameterList.isEmpty() );
+    }
+
+    @Test
+    public void updateServiceInstanceTest(){
+        serviceInstanceProvisioningTest();
+
+        String newTime = "3d 5m";
+
+        UpdateServiceInstanceRequest request = UpdateServiceInstanceRequest.builder()
+                .planId(planId)
+                .serviceInstanceId(siId)
+                .parameters("time", newTime)
+                .build();
+
+        brokerController.updateServiceInstance(request);
+
+        ServiceInstance si = serviceInstanceRepository.findByServiceInstanceId(siId);
+        Assert.assertNotNull(si);
+        Assert.assertEquals(siId, si.getServiceInstanceId());
+        Assert.assertEquals(planId, si.getServicePlanId());
+
+        List<Parameter> parameterList = parameterRepository.findParametersByReference(siId);
+        boolean containsParameterTime = false;
+        for(Parameter p: parameterList) {
+            if (p.getKey().equals("time")) {
+                containsParameterTime = true;
+                Assert.assertEquals(newTime, p.getValue());
+            }
+        }
+        Assert.assertTrue( containsParameterTime );
     }
 
     public void cleanDatabase() {
