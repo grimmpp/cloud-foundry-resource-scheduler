dd5458b735daf6e259fc7086f926f87929b8baa2
Philipp Grimm <grimm.philipp@gmail.com>
2019-03-22 17:58:51 +0100

    additional test added

--- src/main/java/de/grimmpp/AppManager/service/ServicePlanSwitchOffAppsInSpace.java
+++ src/main/java/de/grimmpp/AppManager/service/ServicePlanSwitchOffAppsInSpace.java
@@ -2,40 +2,54 @@ package de.grimmpp.AppManager.service;
 
 import de.grimmpp.AppManager.model.cfClient.Application;
 import de.grimmpp.AppManager.model.cfClient.Resource;
-import de.grimmpp.AppManager.model.cfClient.ServiceInstance;
-import org.springframework.beans.factory.annotation.Autowired;
+import de.grimmpp.AppManager.model.database.Parameter;
+import de.grimmpp.AppManager.model.database.ServiceInstance;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
+@Slf4j
 @Service
-public class ServicePlanSwitchOffAppsInSpace {
+public class ServicePlanSwitchOffAppsInSpace extends IServicePlanBasedOnServiceInstance {
 
     public static final String PLAN_ID = "4e1020f9-6577-4ba3-885f-95bb978b4939";
 
-    @Autowired
-    CfClient cfClient;
+    @Override
+    protected void performActionForServiceInstance(ServiceInstance si) throws IOException {
 
-    public void run() throws IOException {
-        Set<String> spaceIds = new HashSet<>();
-        // Get all service instances for this plan and iterate through ..
-        List<Resource<ServiceInstance>> serviceInstances = cfClient.getServiceInstances(PLAN_ID);
-        for(Resource<ServiceInstance> si: serviceInstances) {
+        String url = cfClient.buildUrl(CfClient.URI_APPS_OF_SPACE, si.getSpaceId());
+        List<Resource<Application>> apps = cfClient.getResources(url, Application.class);
+        log.debug("Collected {} apps from space: {}", apps.size(), PLAN_ID);
 
-            spaceIds.add( si.getEntity().getSpace_guid() );
-        }
+        for (Resource<Application> app : apps) {
 
-        for(String sId: spaceIds) {
-            String url = cfClient.buildUrl(CfClient.URI_APPS_OF_SPACE, sId);
-            List<Resource<Application>> apps = cfClient.getResources(url, Application.class);
+            long currentTime = System.currentTimeMillis();
+            if (app.getEntity().getState().equals("STARTED")) {
+                String logLine = String.format("app: %s, space: %s because of serviceInstance: %s",
+                        app.getMetadata().getGuid(), si.getSpaceId(), si.getServiceInstanceId());
 
-            for(Resource<Application> app: apps) {
-                String appUrl = cfClient.buildUrl(CfClient.URI_SINGLE_APP, app.getMetadata().getGuid());
-                cfClient.updateResource(appUrl, "{\"state\": \"STOPPED\"}", String.class);
+                Parameter p = pRepo.findByReferenceAndKey(si.getServiceInstanceId(), TimeParameterValidator.KEY);
+                long time = TimeParameterValidator.getTimeInMilliSecFromParameterValue(p.getValue());
+
+                long timeDiff = currentTime - app.getMetadata().getUpdated_at().getTime();
+                if (timeDiff > time) {
+                    try {
+                        String appUrl = cfClient.buildUrl(CfClient.URI_SINGLE_APP, app.getMetadata().getGuid());
+                        cfClient.updateResource(appUrl, "{\"state\": \"STOPPED\"}", String.class);
+
+                        log.info("Stopped ");
+                    } catch (Throwable e) {
+                        log.error("Cannot stop " + logLine, e);
+                    }
+                }
             }
         }
+    }
+
+    @Override
+    public String getServicePlanId() {
+        return PLAN_ID;
     }
 }
